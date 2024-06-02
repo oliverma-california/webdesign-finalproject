6f7f022bdd22432ffc17f234f5ef3509b561ebaf
oliverma-california <om86052@yahoo.com>
2024-06-02 04:21:07 -0700

    fix yt iframe rounding in resources

--- src/routes/resources/+page.svelte
+++ src/routes/resources/+page.svelte
@@ -19,14 +19,14 @@
         <li><a href="https://earth911.com" class="font-bold underline text-blue-600">Earth911</a> offers a recycling search tool that helps to find local recycling facilities.</li>
 
         <li><a href="https://recyclenation.com" class="font-bold underline text-blue-600">RecycleNation</a> does a similar thing to Earth911.</li>
-        <iframe class="rounded-lg pt-5 pb-10" width="560" height="315" src="https://www.youtube.com/embed/zykqEP1RMdA?si=cxr8KrnbAyZ7XSPy" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <iframe class="rounded-lg mt-5 mb-10" width="560" height="315" src="https://www.youtube.com/embed/zykqEP1RMdA?si=cxr8KrnbAyZ7XSPy" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 
         <li><span class="font-bold">Local Government Websites</span> often have relevant information for your region on waste management, recycling programs, and sustainability initiatives.</li>
         
         <li><a href="https://nextdoor.com" class="font-bold underline text-blue-600">Nextdoor</a> and <a href="https://www.meetup.com" class="font-bold underline text-blue-600">Meetup</a> are social networking platforms that facilitate communication within a neighborhood for events such as clean-ups and garage sales.</li>
         <div class="flex gap-10">
-          <iframe class="rounded-lg pt-5 pb-10" width="560" height="315" src="https://www.youtube.com/embed/RkeavnLORSE?si=S4O2WE0fdtYrjlkY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-          <iframe class="rounded-lg pt-5 pb-10" width="560" height="315" src="https://www.youtube.com/embed/9XISBrxumb0?si=4AxEUhAchkijOCJb" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          <iframe class="rounded-lg mt-5 mb-10" width="560" height="315" src="https://www.youtube.com/embed/RkeavnLORSE?si=S4O2WE0fdtYrjlkY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          <iframe class="rounded-lg mt-5 mb-10" width="560" height="315" src="https://www.youtube.com/embed/9XISBrxumb0?si=4AxEUhAchkijOCJb" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </div>
 
         <li><a href="https://www.localharvest.org/" class="font-bold underline text-blue-600">Local Harvest</a> helps find farmers markets, family farms, and other sources of sustainably grown food.</li>
